6e79e96a1c23b2ff89cec441c19147e74b951d6d
Reyna Ramirez <ramirezverareyna1@gmail.com>
2021-06-14 11:09:12 -0700

    app.js

--- app.js
+++ app.js
@@ -1,14 +1,25 @@
-const generatePage = require('./src/page-template.js');
+const fs = require('fs');
+const generatePage = require('./src/page-template');
 
+const profileDataArgs = process.argv.slice(2);
 
+console.log(profileDataArgs);
+
+const [name, github] = profileDataArgs;
+
+console.log(name, github);
+
+const pageHTML = generatePage(name, github);
+
+fs.writeFile('./index.html', pageHTML, err => {
+  if (err) throw err;
+
+  console.log('Portfolio complete! Check out index.html to see the output!');
+});
 
 
 // const generatePage = () => 'Name: Jane, Github: janehub';
 // console.log(generatePage());
-
-
-
-
 
 
 // const profileDataArgs = process.argv.slice(2);
--- src/page-template.js
+++ src/page-template.js
@@ -1,22 +1,15 @@
-// THIS FUNCTION IS WRITTEN WITH TEMPLATE LITERALS / ${<variable>} syntax.
-
-const fs = require('fs');
-
-const profileDataArgs = process.argv.slice(2);
-
-const [name, github] = profileDataArgs;
-
-const generatePage = (name, github) => {
+module.exports = (name, github) => {
   return `
-  <!DOCTYPE html> 
-  <html lang="en"> 
+  <!DOCTYPE html>
+  <html lang="en">
+  
   <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Portfolio Demo</title>
   </head>
-
+  
   <body>
     <h1>${name}</h1>
     <h2><a href="https://github.com/${github}">Github</a></h2>
@@ -24,11 +17,3 @@ const generatePage = (name, github) => {
   </html>
   `;
 };
-
-fs.writeFile('./index.html', generatePage(name, github), err => {
-  if (err) throw new Error(err);
-
-  console.log('Portfolio complete! Check out index.html to see the output!');
-});
-
-module.exports = generatePage;
