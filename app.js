cab3cc23c014775669f607621ed7fc56fd970d50
Reyna Ramirez <ramirezverareyna1@gmail.com>
2021-06-14 10:59:54 -0700

    index.html

--- app.js
+++ app.js
@@ -1,30 +1,11 @@
+const generatePage = require('./src/page-template.js');
+
+
+
 
 // const generatePage = () => 'Name: Jane, Github: janehub';
 // console.log(generatePage());
 
-
-// THIS FUNCTION IS WRITTEN WITH TEMPLATE LITERALS / ${<variable>} syntax.
-const generatePage = (userName, githubName) => `Name: ${userName}, Github: ${githubName}`;
-console.log(generatePage('Jane', 'janehub'));
-
-const generatePage = (name, github) => {
-  return `
-  <!DOCTYPE html> 
-  <html lang="en"> 
-  <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Portfolio Demo</title>
-  </head>
-
-  <body>
-    <h1>${name}</h1>
-    <h2><a href="https://github.com/${github}">Github</a></h2>
-  </body>
-  </html>
-  `;
-};
 
 
 
