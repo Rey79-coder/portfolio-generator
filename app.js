bd5b4b723690a79c1feabe6818059a350244a119
Reyna Ramirez <ramirezverareyna1@gmail.com>
2021-06-14 10:00:47 -0700

    app.js

--- app.js
+++ app.js
@@ -1,3 +1,75 @@
+
+// const generatePage = () => 'Name: Jane, Github: janehub';
+// console.log(generatePage());
+
+
+// THIS FUNCTION IS WRITTEN WITH TEMPLATE LITERALS / ${<variable>} syntax.
+const generatePage = (userName, githubName) => `Name: ${userName}, Github: ${githubName}`;
+console.log(generatePage('Jane', 'janehub'));
+
+const generatePage = (name, github) => {
+  return `
+  <!DOCTYPE html> 
+  <html lang="en"> 
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Portfolio Demo</title>
+  </head>
+
+  <body>
+    <h1>${name}</h1>
+    <h2><a href="https://github.com/${github}">Github</a></h2>
+  </body>
+  </html>
+  `;
+};
+
+// const profileDataArgs = process.argv.slice(2);
+
+// const printProfileData = profileDataArr => {
+//   // This...
+//   for (let i = 0; i < profileDataArr.length; i += 1) {
+//     console.log(profileDataArr[i]);
+//   }
+
+//   console.log('================');
+
+  // Is the same as this...
+//   profileDataArr.forEach(profileItem => console.log(profileItem));
+// };
+
+// printProfileData(profileDataArgs);
+
+
+// const generatePage = (userName, githubName) => `Name: ${userName}, Github: ${githubName}`;
+// console.log(generatePage('Jane', 'janehub'));
+
+// const generatePage = () => 'Name: Jane, Github: janehub';
+
+// const generatePage = (userName, githubName) => {
+//     return `
+//       Name: ${userName}
+//       GitHub: ${githubName}
+//     `;
+//   };
+
+
+
+
+
+
+// const generatePage = (userName, githubName) => `Name: ${userName}, Github: ${githubName}`;
+
+// console.log(generatePage('Jane', 'janehub'));
+
+
+
+
+
+
+
 // var message = 'Hello Node!';
 
 // var sum = 5 + 3;
@@ -158,20 +230,3 @@
 //     profileDataArr.forEach(profileItem => console.log(profileItem));
 
 // }
-
-
-const profileDataArgs = process.argv.slice(2);
-
-const printProfileData = profileDataArr => {
-  // This...
-  for (let i = 0; i < profileDataArr.length; i += 1) {
-    console.log(profileDataArr[i]);
-  }
-
-  console.log('================');
-
-  // Is the same as this...
-  profileDataArr.forEach(profileItem => console.log(profileItem));
-};
-
-printProfileData(profileDataArgs);
