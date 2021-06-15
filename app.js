8f75136d443824faa40cbef8e8b25557c9b3a2ff
Reyna Ramirez <ramirezverareyna1@gmail.com>
2021-06-15 15:54:03 -0700

    questions/done

--- app.js
+++ app.js
@@ -39,10 +39,26 @@ const promptUser = () => {
 
 
     {
+      type: 'confirm',
+      name: 'confirmAbout',
+      message: 'Would you like to enter some information about yourself for an "About" section?',
+      default: true
+    },
+
+    {
       type: 'input',
       name: 'about',
-      message: 'Provide some information about yourself:'
+      message: 'Provide some information about yourself:',
+      when: ({ confirmAbout }) => {
+        if (confirmAbout) {
+          return true;
+        } else {
+          console.log('No info about yourself');
+          return false;
+        }
+      }
     }
+
   ]);
 };
 
